5662bafe927f991a8e5537af31577150f65d8fa0
Jessicasgc <jessicaamadearahma@gmail.com>
2025-01-16 20:10:49 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 /* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+//const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 const init = async () => {
+  const notesService = new NotesService();
   const server = Hapi.server(
     {
       port: 5000,
@@ -14,7 +17,15 @@ const init = async () => {
     }
   );
 
-  server.route(routes);
+  //server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
